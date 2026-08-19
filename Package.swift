2d0047f1a17cb3448f3834b290084572afb9432a
Dipak Singh <way2developer.dipak@gmail.com>
2026-08-19 12:41:54 +0530

    Update checksum for NetworkLogger binary target

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .binaryTarget(
             name: "NetworkLogger",
             url: "https://github.com/way2dipak/NetworkLogger/releases/download/1.0.0/NetworkLogger.xcframework.zip",
-            checksum: "0984430b129569dbd32e03f8024bbb63760987c14ec696cc9f30ba25f5be564d"
+            checksum: "ddef3598923bbb81b0c8f94c6fbe1576ae07ac158991521a1d8e7580f418c309"
         )
     ]
 )
