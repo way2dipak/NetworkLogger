9c22680762ed2cf713fbbc14c4fde43e1d6a5682
Dipak Singh <way2developer.dipak@gmail.com>
2026-08-19 11:46:40 +0530

    added package file

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "NetworkLogger",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        .library(
+            name: "NetworkLogger",
+            targets: ["NetworkLogger"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "NetworkLogger",
+            url: "https://github.com/way2dipak/NetworkLogger/releases/download/1.0.0/NetworkLogger.xcframework.zip",
+            checksum: "0984430b129569dbd32e03f8024bbb63760987c14ec696cc9f30ba25f5be564d"
+        )
+    ]
+)
